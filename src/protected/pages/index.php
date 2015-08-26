a6639afcd13d9569d6334e912e0a656c39a0bbed
Andreу Davidovich <cri2net@gmail.com>
2015-08-26 17:57:43 +0300

    запрограммировал слайдер на работу с БД

--- src/protected/pages/index.php
+++ src/protected/pages/index.php
@@ -1,40 +1,40 @@
 <div class="home">
+	<?php
+		$slides = PDO_DB::table_list(TABLE_PREFIX . 'hot_news', 'is_active=1 AND LENGTH(img_filename) > 3', 'pos ASC');
 
+		if ($slides) {
+			?>
+			<slider>
+				<div class="slider-btn next-btn" onclick="next_slide_rotate_index();"></div>
+				<div class="slider-btn prev-btn" onclick="prev_slide_rotate_index();"></div>
+				<?php
+					for ($i=0; $i < count($slides); $i++) {
+						$href = str_ireplace('{SITE_URL}', BASE_URL, $slides[$i]['link']);
+						?>
+						<a id="slide_<?= $i; ?>" href="<?= htmlspecialchars($href, ENT_QUOTES); ?>" style="display:<?= ($i == 0) ? 'block' : 'none'; ?>">
+							<img src="<?= BASE_URL; ?>/db_pic/hot_news/<?= $slides[$i]['img_filename']; ?>" alt="" />
+						</a>
+						<?php
+					}
+				?>
+				<div class="bullets">
+					<?php
+						for ($i=0; $i < count($slides); $i++) {
+							?><div class="bullet <?= ($i == 0) ? 'active' : ''; ?>" id="bullet_<?= $i; ?>" onclick="jump_to_slide(<?= $i; ?>);"></div> <?php
+						}
+					?>
+				</div>
+				<script type="text/javascript">
+					var slide_count = <?= count($slides); ?>;
 
-
-	<slider>
-		<div class="slider-btn next-btn" onclick="next_slide_rotate_index();"></div>
-		<div class="slider-btn prev-btn" onclick="prev_slide_rotate_index();"></div>
-		<a id="slide_0" href="#" style="display:block">
-			<img src="<?= BASE_URL; ?>/db_pic/slide_1.jpg" alt="" />
-		</a>
-		<a id="slide_1" href="#" style="display:none">
-			<img src="<?= BASE_URL; ?>/db_pic/slide_1.jpg" alt="" />
-		</a>
-		<a id="slide_2" href="#" style="display:none">
-			<img src="<?= BASE_URL; ?>/db_pic/slide_1.jpg" alt="" />
-		</a>
-			
-		<div class="bullets">
-			<div class="bullet active" id="bullet_0" onclick="jump_to_slide(0);"></div>
-			<div class="bullet" id="bullet_1" onclick="jump_to_slide(1);"></div>
-			<div class="bullet" id="bullet_2" onclick="jump_to_slide(2);"></div>
-		</div>
-
-		<script type="text/javascript">
-			var slide_count = 3;
-
-			$('.bullets').everyTime(4000, 'bullets_svg', function() {
-				next_slide_rotate_index();
-			});
-		</script>
-	</slider>
-
-
-
-
-
-
+					$('.bullets').everyTime(4000, 'bullets_svg', function() {
+						next_slide_rotate_index();
+					});
+				</script>
+			</slider>
+			<?php
+		}
+	?>
 	<div class="features">
 		<div class="feature left">
 			<div class="title">Сплачуйте за послуги ЖКГ он-лайн</div>
