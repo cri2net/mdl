917fb94edd8114c8a6bb71f237f18ca803fcbc86
Andreу Davidovich <cri2net@gmail.com>
2015-08-27 16:50:55 +0300

    Пример страницы конкретной новости

--- src/protected/scripts/breadcrumbs.php
+++ src/protected/scripts/breadcrumbs.php
@@ -13,6 +13,9 @@
 				case 'page/news':
 					?><span typeof="v:Breadcrumb"><a rel="v:url" property="v:title" href="<?= BASE_URL; ?>/">ГіОЦ</a></span>&nbsp;/&nbsp;<span class="current">Новини</span><?php
 					break;
+				case 'page/news-item':
+					?><span typeof="v:Breadcrumb"><a rel="v:url" property="v:title" href="<?= BASE_URL; ?>/">ГіОЦ</a></span>&nbsp;/&nbsp;<span typeof="v:Breadcrumb"><a rel="v:url" property="v:title" href="<?= BASE_URL; ?>/news/">Новини</a></span>&nbsp;/&nbsp;<span class="current">14 червня 2015</span><?php
+					break;
 
 				case 'error/404':
 					?><span typeof="v:Breadcrumb"><a rel="v:url" property="v:title" href="<?= BASE_URL; ?>/">ГіОЦ</a></span>&nbsp;/&nbsp;<span class="current">Помилка 404</span><?php
