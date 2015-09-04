161c452e568448e0deb9c560d0207fc17cc6e191
Andreу Davidovich <cri2net@gmail.com>
2015-09-04 16:47:41 +0300

    на главной подключил вывод реальных новостей

--- src/protected/pages/index.php
+++ src/protected/pages/index.php
@@ -22,25 +22,38 @@
 		</div>
 	</div>
 
-	<h2 class="big-subtitle">Останнi новини</h1>
-	<div class="news-list">
-		<div class="news-item first">
-			<h2 class="title">Про внесення змiн до норм споживання</h2>
-			<div class="date">15 травня</div>
-			<div class="announce">
-				Кабінет Міністрів України постановив про внесення змін до норм споживання природного газу населенням у разі відсутності газових лічильників. Постанова КМУ від 29.04.2015 № 237
+	<?php
+		$news = PDO_DB::table_list(News::TABLE, "is_actual=1", "created_at DESC", "2");
+		if (count($news) > 0) {
+			?>
+			<h2 class="big-subtitle">Останнi новини</h1>
+			<div class="news-list">
+				<?php
+					for ($i=0; $i < count($news); $i++) {
+						
+						$date = date('d ', $news[$i]['created_at']) . $MONTHS[date('n', $news[$i]['created_at'])]['ua'];
+						if (date('Y') != date('Y', $news[$i]['created_at'])) {
+							$date .= date(' Y', $news[$i]['created_at']);
+						}
+
+						if (mb_strlen($news[$i]['title'], 'UTF-8') > 50) {
+							$news[$i]['title'] = mb_substr($news[$i]['title'], 0, 50, 'UTF-8') . '...';
+						}
+
+						?>
+						<div class="news-item <?= ($i == 0) ? 'first' : ''; ?>">
+							<h2 class="title"><?= htmlspecialchars($news[$i]['title']); ?></h2>
+							<div class="date"><?= $date; ?></div>
+							<div class="announce"><?= ($news[$i]['announce']); ?></div>
+							<div class="details"><a href="<?= BASE_URL; ?>/news/<?= composeURLKey($news[$i]['title']); ?>_<?= $news[$i]['id']; ?>/">детальнiше...</a></div>
+						</div>
+						<?php
+					}
+				?>
 			</div>
-			<div class="details"><a href="#">детальнiше...</a></div>
-		</div>
-		<div class="news-item">
-			<h2 class="title">Куди звертатися для отримання субсидій?</h2>
-			<div class="date">12 квiтня</div>
-			<div class="announce">
-				Процедура отримання субсидій здійснюється в районних управліннях праці та соціального захисту. На дошках оголошень під'їздах житлових будинків розміщено про адреси управлінь та їх контактні телефони.
-			</div>
-			<div class="details"><a href="#">детальнiше...</a></div>
-		</div>
-	</div>
+			<?php
+		}
+	?>
 	<h2 class="big-subtitle green">Останнi матерiали для споживачiв</h1>
 	<div class="news-list">
 		<div class="news-item first">
