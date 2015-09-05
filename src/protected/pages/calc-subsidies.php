5d5a7a636dcd5f9a76a26d236006e5c21c31da3a
Sergey Nalomenko <nalomenko@gmail.com>
2015-09-05 17:34:21 +0300

    Icon changed in subsidies calculator

--- src/protected/pages/calc-subsidies.php
+++ src/protected/pages/calc-subsidies.php
@@ -1,5 +1,4 @@
 <h1>Розрахунок розміру обов’язкового платежу</h1>
-
 <div class="calculator" >
 	<form action="#" method="POST" >
 		<div class="item-row" >
@@ -10,7 +9,7 @@
 			</div>
 		</div>
 		<div class="item-row" >
-			<div class="col-icon" ><img src="<?= BASE_URL ?>/pic/pages/calculator/icon-abacus.png" alt="" /></div>
+			<div class="col-icon" ><img src="<?= BASE_URL ?>/pic/pages/calculator/icon-money.png" alt="" /></div>
 			<div class="col-label" >Кількість осіб зареєстрованих<br/>у житловому приміщенні </div>
 			<div class="col-input" >
 				<input type=text class="txt num-short green bold s24 form-txt-input" value="1" />
