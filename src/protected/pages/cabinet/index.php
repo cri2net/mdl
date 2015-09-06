c5e232669447551ab1033b1a37e7262315cea03b
Sergey Nalomenko <nalomenko@gmail.com>
2015-09-06 17:46:46 +0300

    Personal cabinet unauth: CSS/HTML coding of QuickPays links & text description in the bottom of page

--- src/protected/pages/cabinet/index.php
+++ src/protected/pages/cabinet/index.php
@@ -18,3 +18,22 @@
         </div>
     </div>
 </div>
+<div class="block block--quick-pays">
+    <h2>Миттєвi платежi</h2>
+    <a href="#" class="police" >Cплачуйте штрафи ДАІ</a>
+    <a href="#" class="baby" >Оплата за дитячий садок</a>
+    <a href="#" class="cards" >Переводи з карти на карту</a>
+    <a href="#" class="phone" >Поповнення рахунку<br/>на мобільному</a>
+</div>
+<div class="block block--conclusion" >
+    <h2>Користючись нашим сервісом<br/>
+    ви завжди отримуєте більше</h2>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate 
+    commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam 
+    nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. 
+    Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a 
+    aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, 
+    malesuada quis ornare accumsan, blandit sed diam.
+    </p>
+</div>
