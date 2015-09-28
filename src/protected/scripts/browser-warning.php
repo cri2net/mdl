f1cf19d109b0dd7723df5ba6c42b156776bda0ca
Andreу Davidovich <cri2net@gmail.com>
2015-09-28 13:23:59 +0300

    Исправление отображения иконок браузеров.

--- src/protected/scripts/browser-warning.php
+++ src/protected/scripts/browser-warning.php
@@ -1,15 +1,14 @@
 <?php
-if(!BROWSER_VALID)
-{
-?>
-<div class="old-browsers-warning">
-	<div class="inner" >
-		<b>Попередження:</b> Ви використовуєте застарiлу версiю iнтернет-браузера, що може призвести до некоректного вiдображення сайту.<br/>
-		Рекомендуємо завантажити останню версiю одного з популярних браузерiв
-		<a class="browser firefox" target="_blank" href="https://www.mozilla.org/uk/firefox/new/" >Firefox</a>
-		<a class="browser chrome" target="_blank" href="https://www.google.ru/chrome/browser/desktop/" >Chrome</a>
-		<a class="browser opera" target="_blank" href="http://www.opera.com/uk/computer" >Opera</a>
-	</div>
-</div>
-<?php
+if (!BROWSER_VALID) {
+    ?>
+    <div class="old-browsers-warning">
+        <div class="inner">
+            <b>Попередження:</b> Ви використовуєте застарілу версію інтернет-браузера, що може призвести до некоректного відображення сайту.<br>
+            Рекомендуємо завантажити останню версію одного з популярних браузерів
+            <a class="browser firefox" target="_blank" href="https://www.mozilla.org/uk/firefox/new/">Firefox</a>
+            <a class="browser chrome" target="_blank" href="https://www.google.ru/chrome/browser/desktop/">Chrome</a>
+            <a class="browser opera" target="_blank" href="http://www.opera.com/uk/computer">Opera</a>
+        </div>
+    </div>
+    <?php
 }
