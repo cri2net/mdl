d76415d1155f7126b3f1cfbbfe2c3f3275609e98
Andreу Davidovich <cri2net@gmail.com>
2015-12-08 18:19:40 +0200

    Базовый класс для работы с процессингом банка Крещатик. Временный файл для тестирования работы с банком.

---
+++ src/_test_bank_8583_protocol.php
@@ -0,0 +1,10 @@
+<?php
+require_once(__DIR__ . '/protected/config.php');
+
+
+if (!in_array(USER_REAL_IP, ['46.151.192.106', '195.138.83.178', '127.0.0.1'])) {
+    exit('Access denied');
+}
+
+$JAK8583 = new JAK8583();
+$Khreshchatyk = new Khreshchatyk();
---
+++ src/protected/classes/Khreshchatyk.php
@@ -0,0 +1,14 @@
+<?php
+
+class Khreshchatyk
+{
+    protected $SVFE_host;
+    protected $SVFE_port;
+
+    public function __construct()
+    {
+        $this->SVFE_host = '10.192.1.241';
+        $this->SVFE_port = 12406;
+    }
+
+}
