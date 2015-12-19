72b0819494078018539ad921ae9052643032540e
Andreу Davidovich <cri2net@gmail.com>
2015-12-19 14:43:51 +0200

    Написал метод по получению данных о номере счёта по номеру карты (и номеру паспорта / дате рождения)

--- src/_test_bank_8583_protocol.php
+++ src/_test_bank_8583_protocol.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . '/protected/config.php');
 
 
-if (!in_array(USER_REAL_IP, ['46.151.192.106', '195.138.83.178', '127.0.0.1'])) {
+if (!in_array(USER_REAL_IP, ['46.151.192.106', '195.138.83.178', '127.0.0.1', '195.138.72.249'])) {
     exit('Access denied');
 }
 ?>
@@ -16,6 +16,10 @@ if (!in_array(USER_REAL_IP, ['46.151.192.106', '195.138.83.178', '127.0.0.1'])) 
 <?php
     $jak = new JAK8583();
     $Khreshchatyk = new Khreshchatyk();
+
+    print_r($Khreshchatyk->getCardData('100000000111111', '111111', '01.01.2015'));
+    die();
+
     $iso = $Khreshchatyk->makePayment();
 ?>
 </body>
--- src/protected/classes/Khreshchatyk.php
+++ src/protected/classes/Khreshchatyk.php
@@ -23,6 +23,56 @@ class Khreshchatyk
         // $this->Merchant_ID = 'XETEST000000002';
     }
 
+    public static function get_API_URL($key)
+    {
+        $urls = [];
+
+        if (stristr(API_URL, 'bank.gioc')) {
+            $urls['CARD_BIND_URL'] = '/reports/rwservlet?report=/site_api/get_accbank.rep&destype=Cache&Desformat=xml&cmdkey=rep&cn=';
+        } else {
+            $urls['CARD_BIND_URL'] = '/reports/rwservlet?report=/home/oracle/reports/site_api/get_accbank.rep&destype=Cache&Desformat=xml&cmdkey=rep&cn=';
+        }
+
+        return $urls[$key];
+    }
+
+    /**
+     * Получение данных о карте по номеру с опциональной проверкой по номеру паспорта и дате рождения
+     * Важно! для привязки карты нужно делать проверку, что $pasp_number и $birthday заполнены.
+     * @param  string  $card_number Номер карты
+     * @param  integer $pasp_number номер паспорта человека, 6 цифр (без серии)
+     * @param  string  $birthday    дата рождения человека в формате d.m.Y
+     *
+     * @return accoc array | false
+     */
+    public function getCardData($card_number, $pasp_number = '', $birthday = '')
+    {
+        $url = API_URL . self::get_API_URL('CARD_BIND_URL') . $card_number;
+        if (!empty($pasp_number)) {
+            $url .= '&pasp=' . $pasp_number . '&datebr=' . $birthday;
+        }
+
+        $data = Http::fgets($url);
+        $data = iconv('CP1251', 'UTF-8', $data);
+        $data = str_ireplace('<?xml version="1.0" encoding="WINDOWS-1251"?>', '<?xml version="1.0" encoding="utf-8"?>', $data);
+        $xml = @simplexml_load_string($data);
+
+        if ($xml === false) {
+            return false;
+        }
+
+        $result = [];
+
+        foreach ($xml->xpath("//ROW") as $row) {
+            return [
+                'acc_bank'      => $row->ACCBANK . '',
+                'card_state_id' => $row->CARDSTAEID . ''
+            ];
+        }
+
+        return false;
+    }
+
     private function sendISO($jak_obj)
     {
         $iso = $jak_obj->getMTI() . hex2bin($jak_obj->getBitmap()) . implode($jak_obj->getData());
