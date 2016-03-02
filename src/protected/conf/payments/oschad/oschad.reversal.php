6d056eb1222c2f967c658556b2604e47b8fb68ce
Оловянников Геннадий <olgenur@gmail.com>
2016-03-02 15:09:55 +0200

    ощад: в backref указываю payment-status/ид.платежа/; добавлен ключ user в запрос возврата для безопасности

--- src/protected/conf/payments/oschad/oschad.reversal.php
+++ src/protected/conf/payments/oschad/oschad.reversal.php
@@ -4,9 +4,11 @@ if(!isset($_GET['oid'])) die('order id not set');
 if(!isset($_GET['am'])) die('amount not set');
 if(!isset($_GET['rrn'])) die('rrn not set');
 if(!isset($_GET['intref'])) die('IntRef not set');
+if(!isset($_GET['user']) || $_GET['user'] != 'gena') die('user denied');
 
 require_once('oschad.conf.php');
 $oschad = new Oschad();
+$oschad_merchant_settings['BACKREF'] = 'https://www.gioc.kiev.ua/payment-status/'.((integer)$_GET['oid']);
 $oschad->set_merchant($oschad_merchant_settings);
 $oschad->set_reversal($_GET['oid'], $_GET['am'], $_GET['rrn'], $_GET['intref']);
 $oschad->sign($oschad_sign_key);
