aa918da92adc85a7b9cab847c9282ff5d9281700
Andreу Davidovich <cri2net@gmail.com>
2016-04-26 12:28:49 +0300

    Включил пополнение мобильных в виде фрейма на герц GERC-13

--- src/protected/scripts/cabinet/instant-payments/phone.php
+++ src/protected/scripts/cabinet/instant-payments/phone.php
@@ -1,3 +1,10 @@
 <h1 class="big-title">Поповнення рахунку на мобільному</h1>
 <br><br><br>
-<h2>Розділ у розробці</h2>
+<iframe src="https://www.gerc.ua/mobile.gerc.php?owner=gioc" frameborder="0" style="width:820px; height:995px;"></iframe>
+<script>
+    $(document).keydown(function(e) {
+        if ((e.keyCode == 116) || (e.keyCode == 82 && e.ctrlKey)) {
+            return false;
+        }
+    });
+</script>
--- src/protected/scripts/cabinet/quick-pays.php
+++ src/protected/scripts/cabinet/quick-pays.php
@@ -3,5 +3,5 @@
     <!-- <a href="<?= BASE_URL; ?>/cabinet/instant-payments/dai/" class="police">Cплачуйте штрафи ДАІ</a> -->
     <!-- <a href="<?= BASE_URL; ?>/cabinet/instant-payments/kindergarten/" class="baby">Сплата за дитячий садок</a> -->
     <a href="<?= BASE_URL; ?>/cabinet/instant-payments/cards/" class="cards">Перекази з карти на карту</a>
-    <!-- <a href="<?= BASE_URL; ?>/cabinet/instant-payments/phone/" class="phone">Поповнення рахунку<br>на мобільному</a> -->
+    <a href="<?= BASE_URL; ?>/cabinet/instant-payments/phone/" class="phone last">Поповнення рахунку<br>на мобільному</a>
 </div>
