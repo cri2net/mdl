0f73f822661218e1440662636545ee9da3c8588e
Andrey Davidovich <cri2net@gmail.com>
2016-09-28 00:49:03 +0300

    подключил правильные шапку, подвал и стили

--- src/protected/layouts/_footer.php
+++ src/protected/layouts/_footer.php
@@ -1,28 +1,5 @@
-        </div>
-    </content>
-    <footer>
-    </footer>
-</div>
-<script src="<?= BASE_URL; ?>/js/main.js?m=<?= (is_readable(ROOT . "/js/main.js")) ? filemtime(ROOT . "/js/main.js") : ''; ?>"></script>
-<script src="<?= BASE_URL; ?>/js/jquery-ui.1.10.4.min.js"></script>
-<script src="<?= BASE_URL; ?>/js/jquery.timers.js"></script>
-<script src="<?= BASE_URL; ?>/js/jquery_extends.js"></script>
-<script src="<?= BASE_URL; ?>/js/jquery.maskedinput-1.4.1.js"></script>
-<script src="<?= BASE_URL; ?>/js/jquery.prettyPhoto.js"></script>
-<script src="<?= BASE_URL; ?>/js/orphus.js" defer></script>
-<script src="<?= BASE_URL; ?>/js/jqueryrotate.2.1.js" defer></script>
-<script src="<?= BASE_URL; ?>/js/jquery.easydropdown.min.js"></script>
-<script src="<?= BASE_URL; ?>/js/jquery.tooltipster.min.js"></script>
-<script src="<?= BASE_URL; ?>/js/zxcvbn.js"></script>
 <script>
     var BASE_URL = '<?= BASE_URL ?>';
-</script>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('.tooltip').tooltipster({
-            contentAsHTML: true
-        });
-    });
 </script>
 <?php
     if (USER_REAL_IP !== '127.0.0.1') {
--- src/protected/layouts/_header.php
+++ src/protected/layouts/_header.php
@@ -5,92 +5,28 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <!--[if lt IE 9]><style> input.txt, textarea.txt, select.txt { border:1px solid #444 !important; }</style><![endif]-->
 <!--[if lt IE 9]><script>var e = ("breadcrumbs,slider,slide,submenu,heading,info,sidebar,menu,header,footer,news,content").split(','); for (var i = 0; i < e.length; i++) {document.createElement(e[i]);}</script><![endif]-->
-<link href="<?= BASE_URL; ?>/style/style.css?m=<?= (is_readable(ROOT . "/style/style.css")) ? filemtime(ROOT . "/style/style.css") : ''; ?>" rel="stylesheet" type="text/css" />
-<?php
-	$file = ROOT . '/style/style-custom.css';
-	if (file_exists($file) && (filesize($file) > 0)) {
-		?>
-		<link href="<?= BASE_URL; ?>/style/style-custom.css?m=<?= (is_readable(ROOT . "/style/style-custom.css")) ? filemtime(ROOT . "/style/style-custom.css") : ''; ?>" rel="stylesheet" type="text/css" />
-		<?php
-	}
-?>
+<link href="<?= BASE_URL; ?>/css/style.css?m=<?= (is_readable(ROOT . "/style/style.css")) ? filemtime(ROOT . "/style/style.css") : ''; ?>" rel="stylesheet" type="text/css" />
 <title><?php require_once(ROOT . "/protected/scripts/seo/title.php"); ?></title>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic&amp;subset=latin,cyrillic">
 <?php
-	if (USER_REAL_IP === '127.0.0.1') {
-		?><script type="text/javascript" src="http://localhost:35729/livereload.js"></script> <?php
-	} else {
-		require_once(ROOT . '/protected/scripts/google-analytics.php');
-	}
+    if (USER_REAL_IP === '127.0.0.1') {
+        ?><script type="text/javascript" src="http://localhost:35729/livereload.js"></script> <?php
+    } else {
+        require_once(ROOT . '/protected/scripts/google-analytics.php');
+    }
 ?>
 <script src="<?= BASE_URL; ?>/js/jquery-1.7.2.min.js"></script>
+
+<script src="<?= BASE_URL; ?>/js/main.js"></script>
+<script src="<?= BASE_URL; ?>/js/jquery-ui.1.10.4.min.js"></script>
+<script src="<?= BASE_URL; ?>/js/jquery.timers.js"></script>
+<script src="<?= BASE_URL; ?>/js/jquery_extends.js"></script>
+<script src="<?= BASE_URL; ?>/js/jquery.maskedinput-1.4.1.js"></script>
+<script src="<?= BASE_URL; ?>/js/jquery.prettyPhoto.js"></script>
+<script src="<?= BASE_URL; ?>/js/orphus.js" defer></script>
+<script src="<?= BASE_URL; ?>/js/jqueryrotate.2.1.js" defer></script>
+<script src="<?= BASE_URL; ?>/js/jquery.easydropdown.min.js"></script>
+<script src="<?= BASE_URL; ?>/js/jquery.tooltipster.min.js"></script>
+<script src="<?= BASE_URL; ?>/js/zxcvbn.js"></script>
 </head>
 <body>
-	<div class="main-conteiner">
-		<header>
-			<div class="top-line">
-				<div class="inner">
-					<a class="logo" href="<?= BASE_URL . '/'; ?>"></a>
-					<div class="phone-block">
-						<div class="phone-icon"></div>
-						<div class="number">
-							<?= $_tmp['HEADER_PHONE']; ?>
-							<div class="darr-border"></div>
-							<div class="darr"></div>
-						</div>
-						<div class="full-phone-box">
-							<?= $_tmp['HEADER_PHONE_SECOND']; ?>
-							<div class="work">
-								<div class="work-content">
-									<?= $_tmp['HEADER_WORK']; ?>
-									<div class="line">
-										<div class="col">&nbsp;</div>
-										<div class="col-r"><a href="<?= BASE_URL; ?>/contacts/#page-map-clock">Детальніше</a></div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<?php
-						if (Authorization::isLogin()) {
-							?>
-							<div class="account-link logged-in">
-								<div class="account-open">
-									<div class="head-line">
-										<div class="avatar">
-											<div class="default-avatar"></div>
-											<!-- <img src="http://placehold.it/34x34" alt=""> -->
-										</div>
-										<div class="username"><span><?= htmlspecialchars($__userData['name']) . '&nbsp;' . htmlspecialchars($__userData['lastname']); ?></span></div>
-									</div>
-									<div class="item objects">
-										<a href="<?= BASE_URL; ?>/cabinet/objects/">Об’єкти</a>
-									</div>
-									<div class="item bank">
-										<a href="<?= BASE_URL; ?>/cabinet/payments/">Мої платежі</a>
-									</div>
-									<div class="item flash">
-										<a href="<?= BASE_URL; ?>/cabinet/instant-payments/">Миттєві платежі</a>
-									</div>
-									<div class="item settings with-border">
-										<a href="<?= BASE_URL; ?>/cabinet/settings/">Налаштування профілю</a>
-									</div>
-									<div class="item faq with-border">
-										<a href="<?= BASE_URL; ?>/help/faq_cabinet/">Онлайн довідка</a>
-									</div>
-									<div class="item logout with-border">
-										<a href="<?= BASE_URL; ?>/post/cabinet/logout/">Вихід</a>
-									</div>
-								</div>
-							</div>
-							<?php
-						} else {
-							?><a href="<?= BASE_URL; ?>/cabinet/" class="account-link"><span class="border-bot">Особистий кабінет</span></a> <?php
-						}
-					?>
-				</div>
-			</div>
-		</header>
-		<content>
-			<div class="inner <?= (defined('HAVE_SIDEBAR') && !HAVE_SIDEBAR) ? 'wo-sidebar' : ''; ?> <?= $__route_result['controller'] . '_' . $__route_result['action']; ?>">
-				<?php require_once(ROOT . '/protected/scripts/breadcrumbs.php'); ?>
