228330ba950018007c0deb7a88fc5bcd63a8baeb
Andrey Davidovich <cri2net@gmail.com>
2016-09-29 17:07:24 +0300

    включил пока только один мгновенный платёж = цкс залил шрифт open sans

--- src/protected/scripts/cabinet/quick-pays.php
+++ src/protected/scripts/cabinet/quick-pays.php
@@ -2,10 +2,10 @@
     <h1 class="big-title">Миттєві платежі</h1>
 </div>
 <div class="block block-quick-pays-short">
-    <a href="<?= BASE_URL; ?>/cabinet/instant-payments/cards/" class="cards">Перекази з карти на карту</a>
+    <!-- <a href="<?= BASE_URL; ?>/cabinet/instant-payments/cards/" class="cards">Перекази з карти на карту</a>
     <a href="<?= BASE_URL; ?>/cabinet/instant-payments/phone/" class="phone">Поповнення рахунку на мобільному</a>
     <a href="<?= BASE_URL; ?>/cabinet/instant-payments/dai/" class="police">Cплачуйте штрафи ДАІ</a>
     <a href="<?= BASE_URL; ?>/cabinet/instant-payments/kindergarten/" class="baby">Сплата за дитячий садок</a>
-    <a href="<?= BASE_URL; ?>/cabinet/instant-payments/budget/" class="budget">Платежі до бюджету</a>
+    <a href="<?= BASE_URL; ?>/cabinet/instant-payments/budget/" class="budget">Платежі до бюджету</a> -->
     <a href="<?= BASE_URL; ?>/cabinet/instant-payments/cks/" class="cks">Сплата послуг ЦКС</a>
 </div>
