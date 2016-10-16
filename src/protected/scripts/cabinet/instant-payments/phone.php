ecff2b8e436199302ba19604796ecfcf1cbafb1d
Andrey Davidovich <cri2net@gmail.com>
2016-10-16 13:47:04 +0300

    Вывод пополнения мобильных и p2p CKS-5

--- src/protected/scripts/cabinet/instant-payments/phone.php
+++ src/protected/scripts/cabinet/instant-payments/phone.php
@@ -1,6 +1,6 @@
 <h1 class="big-title">Поповнення рахунку на мобільному</h1>
 <br><br><br>
-<iframe src="https://www.gerc.ua/mobile.gerc.php?owner=gioc" frameborder="0" style="width:820px; height:995px;"></iframe>
+<iframe src="https://www.gerc.ua/mobile.gerc.php?owner=cks" frameborder="0" style="width:820px; height:995px;"></iframe>
 <script>
     $(document).keydown(function(e) {
         if ((e.keyCode == 116) || (e.keyCode == 82 && e.ctrlKey)) {
