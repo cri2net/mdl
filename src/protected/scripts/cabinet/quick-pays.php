c603211b659effdec60dd344b139de88f0bcf262
Andrey Davidovich <cri2net@gmail.com>
2016-12-06 12:33:32 +0200

    Сменил название для Воли GERC-48

--- src/protected/scripts/cabinet/quick-pays.php
+++ src/protected/scripts/cabinet/quick-pays.php
@@ -8,5 +8,5 @@
     <a href="<?= BASE_URL; ?>/cabinet/instant-payments/cards/" class="cards">Перекази з карти на карту</a>
     <a href="<?= BASE_URL; ?>/cabinet/instant-payments/phone/" class="phone">Поповнення рахунку на мобільному</a>
     <a href="<?= BASE_URL; ?>/cabinet/instant-payments/budget/" class="budget">Платежі до бюджету</a>
-    <a href="<?= BASE_URL; ?>/cabinet/instant-payments/volia/" class="volia">Воля</a>
+    <a href="<?= BASE_URL; ?>/cabinet/instant-payments/volia/" class="volia">Поповнення рахунку ВОЛЯ (телебачення та інтернету)</a>
 </div>
