c92d3325eab00a884e4ee454bec4a56467aa2188
Andrey Davidovich <cri2net@gmail.com>
2017-09-01 11:14:55 +0300

    Исправление суммы для webmoney

--- src/protected/conf/payments/webmoney/webmoney.payform.php
+++ src/protected/conf/payments/webmoney/webmoney.payform.php
@@ -1,4 +1,4 @@
-<input type="hidden" name="LMI_PAYMENT_AMOUNT" value="<?= $totalAmountKop; ?>">
+<input type="hidden" name="LMI_PAYMENT_AMOUNT" value="<?= $totalAmount; ?>">
 <input type="hidden" name="LMI_PAYMENT_DESC_BASE64" value="<?= base64_encode("Сплата комунальних послуг"); ?>">
 <input type="hidden" name="LMI_PAYMENT_NO" value="<?= $_payment['id']; ?>">
 <input type="hidden" name="LMI_PAYEE_PURSE" value="<?= LMI_PAYEE_PURSE; ?>">
