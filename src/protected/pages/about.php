8d77043d2d27f0192ee7d395edc7b072b0fc00fb
Sergey Nalomenko <nalomenko@gmail.com>
2017-09-19 11:35:16 +0300

    - Service centers on map @ about page - Webcams test page

--- src/protected/pages/about.php
+++ src/protected/pages/about.php
@@ -1,3 +1,19 @@
+<?php
+    use cri2net\php_pdo_db\PDO_DB;
+
+    $list = PDO_DB::table_list(TABLE_PREFIX . 'service_centers');
+    $markers = [];
+    foreach($list as $sc) {
+        $id = $sc['id'];
+        $region = PDO_DB::row_by_id(TABLE_PREFIX . 'dict_regions', $sc['id_region']);
+        $latitude = $sc['latitude'];
+        $longtitude = $sc['longtitude'];
+        $title = htmlspecialchars($region['title']) . ' р-н';
+        $address = str_replace("'", "\'", $sc['address']);
+        if($latitude&&$longtitude)
+            $markers[]="createMarker('$latitude', '$longtitude'," . ($sc['is_online'] ? 'true' : 'false') . " ,'$title', '<div style=\"max-width:350px;\"><h3>$title</h3>$address</div>')";
+    }
+?>
 <body>
 
 <?php require_once(PROTECTED_DIR . '/layouts/navbar_inner.php'); ?>
@@ -25,7 +41,7 @@
         <div class="about-form">
             <div class="row">
                 <div class="col-md-8">
-                    <div class="map-about matchHeight" id="map" data-lat="50.4359167" data-lng="30.5420269" data-zoom="17"></div>
+                    <div class="map-about matchHeight" id="map_canvas" data-lat="50.4359167" data-lng="30.5420269" data-zoom="17"></div>
                 </div>
                 <div class="col-md-4">
                     <div class="info matchHeight">
@@ -57,3 +73,49 @@
         </div>
     </content>
 </div>
+<script type="text/javascript">
+    function initSCMap()
+    {
+        var myLatlng = new google.maps.LatLng(46.551878, 33.6858659);
+        var myOptions = {
+            zoom: 8,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: false
+        }
+        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    function createMarker(lat, lon, online, title, html) {
+        var newmarker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, lon),
+            map: map,
+            //icon: online ? '/pic/placemark-online.png' : '/pic/placemark-offline.png',
+            title: title
+        });
+        newmarker['infowindow'] = new google.maps.InfoWindow({
+                content: html
+            });
+        google.maps.event.addListener(newmarker, 'click', function() {
+            this['infowindow'].open(map, this);
+            if (typeof currentInfoWindow == "object") {
+                currentInfoWindow.close();
+            }
+            currentInfoWindow = this['infowindow'];
+
+        });
+        return newmarker;
+    }
+     //Add a marker clusterer to manage the markers.
+     var markers = [<?= implode(",", $markers); ?>];
+     var bounds = new google.maps.LatLngBounds();
+     for (var i = 0; i < markers.length; i++) {
+      bounds.extend(markers[i].getPosition());
+     }
+
+     map.fitBounds(bounds);
+    }
+
+    $(function(){
+        initSCMap();
+    });
+
+</script>
