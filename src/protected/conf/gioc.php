8b88216f5afbd36aa2d0cdcee0b82c320c746891
Andrey Davidovich <cri2net@gmail.com>
2018-08-02 10:15:55 +0300

    Смена ссылка на gioc api GERC-122

--- src/protected/conf/gioc.php
+++ src/protected/conf/gioc.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'api_url'   => 'https://gioc.kiev.ua/api/',
+    'api_url'   => 'https://www.gioc.kiev.ua/api/',
     'api_token' => 'gioc-kmda-for-user-flats-sjoisjuwf45eg1dbe87gw287hrt84hb8b54rt',
 ];
