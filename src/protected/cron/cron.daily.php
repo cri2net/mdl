8610787a543925216e0c1c2db0b0c0a1c3622cdb
Andrey Davidovich <cri2net@gmail.com>
2019-05-02 13:14:24 +0300

    Выпилил отчёты по TAS GERC-119

--- src/protected/cron/cron.daily.php
+++ src/protected/cron/cron.daily.php
@@ -6,14 +6,5 @@ error_log(date('Y.m.d H:i:s') . "\r\n", 3, PROTECTED_DIR . '/logs/cron.daily.sta
 ini_set('max_execution_time', 0);
 ini_set('memory_limit', -1);
 
-if (date('d') == '01') {
-    $first_day = new DateTime('first day of previous month 00:00:00');
-} else {
-    $first_day = new DateTime('first day of this month 00:00:00');
-}
-
 Street::cron();
 Authorization::cron();
-
-$today = new DateTime('today');
-CronTasks::sendReportAboutTasLink(date_timestamp_get($first_day), date_timestamp_get($today) - 1);
