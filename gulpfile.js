a89695c9e7aed609ed35a71b6c973f01c83d54f5
Andrey Davidovich <cri2net@gmail.com>
2020-03-24 18:46:01 +0200

    Обновление зависимостей npm & composer

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,12 @@
-var gulp      = require('gulp'),
-    uglify    = require('gulp-uglify'),
-    connect   = require('gulp-connect'),
-    livereload   = require('gulp-livereload'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),    
-    less      = require('gulp-less');
+var gulp    = require('gulp'),
+    uglify  = require('gulp-uglify'),
+    connect = require('gulp-connect'),
+    concat  = require('gulp-concat'),
+    rename  = require('gulp-rename'),    
+    less    = require('gulp-less');
 
 var WEBPAGES_MASK = [
     './src/**/.htaccess',
-    './src/manifest.json',
-    './src/browserconfig.xml',
-    './src/robots.txt',
     './src/**/{*.php,*.html}',
      './src/{script,config,lib,auth,splash,protected}/**/*.*',
     '!./src/protected/**/{*.css,*.js}',
@@ -31,71 +27,51 @@ var CSS_MASK = ['./src/{js,css}/**/*.css'];
 
 /* TASK: Moving PHP & HTML files to public directory */
 gulp.task('webpages', function(){
-    gulp.src(WEBPAGES_MASK)
+    return gulp.src(WEBPAGES_MASK)
         .pipe(gulp.dest('./public'))
         .pipe(connect.reload())
-
-    .on('error', function(err){
-       console.error('error: ' + err.message);
-       console.error(err.stack);
-    });
+        .on('error', function(err){
+            console.error(err);
+        })
 });
 
 /* TASK: Compile LESS files in single CSS */
 gulp.task('less', wrapPipe(function(success, error) {
     return gulp.src('./src/less/*.less')
-    .pipe(
-        less({
-            compress: true,
-            javascriptEnabled: true
+        .pipe(
+            less({
+                compress: true,
+                javascriptEnabled: true
+            })
+        )
+        .pipe(gulp.dest('./public/assets/css'))
+        .pipe(connect.reload())
+        .on('error', function(err) {
+            console.error(err);
         })
-    )
-    .pipe(gulp.dest('./public/assets/css'))
-    .pipe(livereload());
 }));
 
-/* TASK: Compile LESS files in single CSS */
-gulp.task('bootstrap', wrapPipe(function(success, error) {
-    return gulp.src('./src/less/bootstrap/bootstrap.less')
-    .pipe(
-        less({compress: false})
-        .on('error', error)
-    )
-     .pipe(gulp.dest('./public/assets/css'))
-     .pipe(connect.reload());
-}));
-    
-/* TASK: Moving CSS files to public directory */
-/*
-gulp.task('css', function(){
-    gulp.src(CSS_MASK)
-        .pipe(uglify())
-        .pipe(gulp.dest('./public/'))
-});
-*/
 /* TASK: Moving JS files to public/css directory */
 gulp.task('css', function(){
-    gulp.src('./src/css/*.css')
+    return gulp.src('./src/css/*.css')
         .pipe(gulp.dest('./public/assets/css/'))
         .pipe(connect.reload())
 });
 
 /* TASK: Moving JS files to public/js directory */
 gulp.task('js', function(){
-    try {
-        gulp.src(['./src/js/**/*.js', '!./src/js/**/scripts.js'])
-            .pipe(uglify())
-            .pipe(gulp.dest('./public/assets/js/'))
-            .pipe(connect.reload())
-    } catch (e) {
-       console.error('error: ' + e.message);
-       console.error(e.stack);
-    }
+    return gulp.src(['./src/js/**/*.js', '!./src/js/**/scripts.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/assets/js/'))
+        .pipe(connect.reload())
+        .on('error', function(err) {
+            console.error(err);
+        })
 });
 
 var jsFiles = [
         './src/js/vendors/jquery.min.js',
-        './src/js/vendors/jquery-ui.js',
+        './src/js/vendors/jquery-ui.1.10.4.min.js',
         './src/js/vendors/*.js'],
     jsDest = './public/assets/js/';
 
@@ -104,60 +80,56 @@ gulp.task('js_all', function() {
         .pipe(concat('plugins.js'))
         .pipe(gulp.dest(jsDest))
         .pipe(rename('plugins.min.js'))
-
-        .pipe(uglify({
-            preserveComments:'license'
-        }))
-
+        .pipe(uglify())
         .pipe(gulp.dest(jsDest));
 });
 
 /* TASK: Moving JS files to public/js directory */
 gulp.task('js_script', function(){
-    try {
-        gulp.src(['./src/js/**/scripts.js', './src/js/main.js', './src/js/modernizr-2.6.2.min.js'])
+    return gulp.src(['./src/js/**/scripts.js', './src/js/main.js', './src/js/modernizr-2.6.2.min.js'])
+            .pipe(uglify())
             .pipe(gulp.dest('./public/assets/js/'))
             .pipe(connect.reload())
-    } catch (e) {
-       console.error('error: ' + e.message);
-       console.error(e.stack);
-    }
+            .on('error', function(err) {
+                console.error(err);
+            })
 });
 
 
 /* TASK: Moving images (only external, not CSS-encoded) to public/images directory */
 gulp.task('images', function(){
-    gulp.src(IMAGES_MASK)
+    return gulp.src(IMAGES_MASK)
         .pipe(gulp.dest('./public/assets'))
         .pipe(connect.reload())
 })
 
 /* TASK: Moving fonts to public/style/fonts directory */
 gulp.task('fonts', function(){
-    gulp.src(FONTS_MASK)
+    return gulp.src(FONTS_MASK)
         .pipe(gulp.dest('./public/assets'))
         .pipe(connect.reload())
 })
 
 /* TASK: Create webserver for LiveReload */
-gulp.task('connect', function () {
+gulp.task('connect', function (callback) {
     connect.server({
         root: ['public'],
         host: 'localhost',
         port: 8013,
         livereload: true
     });
+    callback();
 });
 
 /* TASK: Watching changes in all-source & images */
-gulp.task('watch', function(){
+gulp.task('watch', function(callback){
+    gulp.watch(WEBPAGES_MASK, gulp.series('webpages'));
+    gulp.watch(['./src/less/**/*.less'], gulp.series('less'));
+    gulp.watch(IMAGES_MASK, gulp.series('images'));
+    gulp.watch('./src/css/*.css', gulp.series('css'));
+    gulp.watch(JS_MASK, gulp.series('js_script'));
 
-    livereload.listen();    
-    gulp.watch(WEBPAGES_MASK, ['webpages']);
-    gulp.watch(['./src/less/**/*.less'], ['less']);
-    gulp.watch(IMAGES_MASK, ['images']);
-    gulp.watch('./src/css/*.css', ['css']);
-    gulp.watch(JS_MASK, ['js_script']);
+    callback();
 });
 
 /**
@@ -170,23 +142,19 @@ gulp.task('watch', function(){
  * }));
  */ 
 function wrapPipe(taskFn) {
-  return function(done) {
-    var onSuccess = function() {
-      done();
-    };
-    var onError = function(err) {
-      done(err);
+    return function(done) {
+        var onSuccess = function() {
+            done();
+        };
+        var onError = function(err) {
+            done(err);
+        }
+        var outStream = taskFn(onSuccess, onError);
+        if (outStream && typeof outStream.on === 'function') {
+            outStream.on('end', onSuccess);
+        }
     }
-    var outStream = taskFn(onSuccess, onError);
-    if(outStream && typeof outStream.on === 'function') {
-      outStream.on('end', onSuccess);
-    }
-  }
 }
 
 /* DEFAULT TASK, running needed tasks */
-//gulp.task('default', ['connect', 'less', 'webpages', 'js', 'css', 'fonts', 'images', 'watch']);
-//gulp.task('default', ['css', 'less', 'webpages', 'js', 'js_all', 'js_script', 'images', 'fonts', 'watch']);
-//gulp.task('default', ['css', 'less', 'webpages', 'js']);
-gulp.task('default', ['css', 'less', 'webpages', 'js_script', 'js_all', 'images', 'fonts', 'watch']);
-
+gulp.task('default',  gulp.parallel('css', 'less', 'webpages', 'js_script', 'js_all', 'images', 'fonts', 'watch'));
