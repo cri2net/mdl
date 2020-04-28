30668b1de91afc23fab76e8cbf5a3105c69d1d82
Andrey Davidovich <cri2net@gmail.com>
2020-04-28 13:59:08 +0300

    mini fix страницы restore

--- src/protected/pages/cabinet/restore.php
+++ src/protected/pages/cabinet/restore.php
@@ -30,37 +30,40 @@
                         ?><br> Перейти до <a href="<?= $_SESSION['restore']['email_link']['link']; ?>"><?= $_SESSION['restore']['email_link']['title']; ?></a> <?php
                     }
                     unset($_SESSION['restore']['status']);
-                    return;
-                }
+                } else {
 
-                $_email = (isset($_SESSION['restore']['email']))
-                    ? $_SESSION['restore']['email']
-                    : ((isset($_SESSION['login']['email'])) ? $_SESSION['login']['email'] : '');
-                $_email = htmlspecialchars($_email, ENT_QUOTES);
+                    $_email = (isset($_SESSION['restore']['email']))
+                        ? $_SESSION['restore']['email']
+                        : ((isset($_SESSION['login']['email'])) ? $_SESSION['login']['email'] : '');
+                    $_email = htmlspecialchars($_email, ENT_QUOTES);
+                    
+                    ?>
+                    <div class="form-group form__group form__group--outer">
+                        <label class="form__label"><span>Електронна пошта / логін </span> <br>
+                            <input type="text"
+                                   name="email"
+                                   value="<?= $_email; ?>"
+                                   class="form__input">
+                        </label>
+                    </div>
+                    <div class="bracket form__group form__group--bracket">або</div>
+                    <div class="form-group form__group form__group--outer">
+                        <label class="form__label">Телефон <br>
+                            <input placeholder="+380"
+                                   type="text"
+                                   name="phone"
+                                   value="<?= $_phone; ?>"
+                                   class="form__input">
+                        </label>
+                    </div>
+                    <div class="form-group form__group">
+                        <button class="button button__form">
+                            Далі
+                        </button>
+                    </div>
+                    <?php
+                }
             ?>
-            <div class="form-group form__group form__group--outer">
-                <label class="form__label"><span>Електронна пошта / логін </span> <br>
-                    <input type="text"
-                           name="email"
-                           value="<?= $_email; ?>"
-                           class="form__input">
-                </label>
-            </div>
-            <div class="bracket form__group form__group--bracket">або</div>
-            <div class="form-group form__group form__group--outer">
-                <label class="form__label">Телефон <br>
-                    <input placeholder="+380"
-                           type="text"
-                           name="phone"
-                           value="<?= $_phone; ?>"
-                           class="form__input">
-                </label>
-            </div>
-            <div class="form-group form__group">
-                <button class="button button__form">
-                    Далі
-                </button>
-            </div>
         </form>
         <a href="<?= BASE_URL; ?>/cabinet/registration/"
            class="button button__form button__form--register button__form--register--outer">
